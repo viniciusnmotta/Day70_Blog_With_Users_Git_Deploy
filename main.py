f012ba170fdcb2a3850b08bb046bcbfa86c12451
vinicius <vinicius.n.motta@gmail.com>
2021-01-30 17:52:57 -0500

    Change contact to print name

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
